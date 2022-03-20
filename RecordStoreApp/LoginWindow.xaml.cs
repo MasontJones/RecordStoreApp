92aece810b864b4a5d8a7c56e7d28d284364fa66
Mason Jones <masonjones410@gmail.com>
2022-03-19 19:42:18 -0500

    Got the Model folder and employee done login is good just need to connect it to the main menu view when its up also got the db connection class.

--- RecordStoreApp/LoginWindow.xaml.cs
+++ RecordStoreApp/LoginWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Data;
+using MySql.Data.MySqlClient;
+using RecordStoreApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace RecordStoreApp
 {
     /// <summary>
@@ -20,9 +25,60 @@ namespace RecordStoreApp
     /// </summary>
     public partial class LoginWindow : Window
     {
+
         public LoginWindow()
         {
+
             InitializeComponent();
+
         }
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dbCon = DBConnection.Instance();
+            dbCon.Server = "209.106.201.103";
+            dbCon.DatabaseName = "group3";
+            dbCon.UserName = "dbstudent6";
+            dbCon.Password = "freshsugar87";
+            if (dbCon.IsConnect())
+            {
+                string enteredUser = UserName.Text;
+                string EnteredPassword = PasswordEntered.Password;
+                SHA1Managed sha1 = new SHA1Managed();
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(EnteredPassword));
+                var sb = new StringBuilder(hash.Length * 2);
+                foreach (byte b in hash)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+                String HashedPassword = sb.ToString(); 
+                     string query = $"SELECT firstName, password FROM Employee WHERE firstName = '{enteredUser}' ";
+                var cmd = new MySqlCommand(query, DBConnection.Connection);
+                var reader = cmd.ExecuteReader();
+                Employee loginemployee = new Employee();
+                while (reader.Read())
+                {
+                   
+                    string FirstName = reader.GetString(0);
+                    string PasswordHash = reader.GetString(1);
+                    loginemployee.FirstName = FirstName;
+                    loginemployee.Password = PasswordHash;
+                }
+
+                if (enteredUser == loginemployee.FirstName && HashedPassword == loginemployee.Password)
+                {
+                    this.Visibility = Visibility.Collapsed;
+                    MerchSearchWindow window = new MerchSearchWindow();
+                    window.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    MessageBox.Show("Password or username was incorrect");
+
+                }
+                reader.Close();             
+            }
+
+        }
+
     }
 }
---
+++ RecordStoreApp/Model/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecordStoreApp.Model
+{
+    class Employee
+    {
+        public int ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Postion { get; set; }
+        public int ReportsTo { get; set; }
+        public string HireDate { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public override string ToString()
+        {
+            return $"{FirstName}, {LastName}";
+        }
+    }
+}
